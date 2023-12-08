9279b53a2868b001d63e14d1e93f5db118e80d04
Muhammad Najimov <mnajimov.unlimit@gmail.com>
2023-12-08 22:44:32 +0500

    Profile fuctionalities done TODO:navigation part and edit profile

--- lib/domain/Profile.dart
+++ lib/domain/Profile.dart
@@ -19,55 +19,111 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: const Text('NOZIMJON NABIYEV'),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(context, '/edit_profile');
+          },
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 20.0,
+              // Adjust the size of the CircleAvatar
+              backgroundColor: Colors.transparent,
+              // Use a transparent background
+              backgroundImage: AssetImage(
+                'lib/images/capybara.png',
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 100.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const CircleAvatar(
-              radius: 80,
-              backgroundImage: AssetImage('lib/images/capybara.png'),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          FunctionalityCard(
+            title: 'Past Tours',
+            icon: Icons.home,
+            onTap: () {
+              // Add navigation logic for buying a property
+              print('Navigate to Buy a Property');
+            },
+          ),
+          FunctionalityCard(
+            title: 'Open House Schedule',
+            icon: Icons.monetization_on,
+            onTap: () {
+              // Add navigation logic for selling a property
+              print('Navigate to Sell a Property');
+            },
+          ),
+          FunctionalityCard(
+            title: 'Recently Viewed',
+            icon: Icons.home,
+            onTap: () {
+              // Add navigation logic for buying a property
+              print('Navigate to Buy a Property');
+            },
+          ),
+          FunctionalityCard(
+            title: 'Saved Searches',
+            icon: Icons.home,
+            onTap: () {
+              // Add navigation logic for buying a property
+              print('Navigate to Buy a Property');
+            },
+          ),
+          FunctionalityCard(
+            title: 'Viva Premier',
+            icon: Icons.home,
+            onTap: () {
+              // Add navigation logic for buying a property
+              print('Navigate to Buy a Property');
+            },
+          ),
+          FunctionalityCard(
+            title: 'Sell Your Home',
+            icon: Icons.home,
+            onTap: () {
+              // Add navigation logic for buying a property
+              print('Navigate to Buy a Property');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FunctionalityCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final VoidCallback onTap;
+
+  FunctionalityCard(
+      {required this.title, required this.icon, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 90.0,
+      width: double.infinity,
+      // Makes the card take the full width of the screen
+      margin: EdgeInsets.only(bottom: 15.0),
+      child: Card(
+        elevation: 2.0,
+        child: ListTile(
+          leading: Icon(
+            icon,
+            size: 35.0,
+          ),
+          title: Center(
+            child: Text(
+              title,
+              style: TextStyle(fontSize: 19.0),
             ),
-            const SizedBox(height: 8),
-            Text(
-              'FirstName: $firstName',
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 8),
-            const SizedBox(height: 8),
-            Text(
-              'LastName: $lastName',
-              style: const TextStyle(fontSize: 18),
-            ),
-            Text(
-              'Username: $username',
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Email: $email',
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SettingsPage(
-                      username: username,
-                      firstName: firstName,
-                      lastName: lastName,
-                      email: email,
-                    ),
-                  ),
-                );
-              },
-              child: const Text('Settings'),
-            ),
-          ],
+          ),
+          onTap: onTap,
         ),
       ),
     );
